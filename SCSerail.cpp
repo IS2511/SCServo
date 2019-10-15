404544a5482842569c902de3d9660dfbd7acd22c
IS2511 <is2511@bk.ru>
2019-10-15 17:15:04 +0300

    Added micros() support Define SCS_USE_MICROS if you want to use micros() instead of millis() in the library. (Maybe needed on not AVR boards)

--- SCSerail.cpp
+++ SCSerail.cpp
@@ -30,7 +30,11 @@ int SCSerail::readSCS(unsigned char *nDat, int nLen)
 {
 	int Size = 0;
 	int ComData;
-	unsigned long t_begin = millis();
+	#if defined(SCS_USE_MICROS)
+		unsigned long t_begin = micros();
+	#else
+		unsigned long t_begin = millis();
+	#endif
 	unsigned long t_user;
 	while(1){
 		ComData = pSerial->read();
@@ -39,12 +43,20 @@ int SCSerail::readSCS(unsigned char *nDat, int nLen)
 				nDat[Size] = ComData;
 			}
 			Size++;
-			t_begin = millis();
+			#if defined(SCS_USE_MICROS)
+				t_begin = micros();
+			#else
+				t_begin = millis();
+			#endif
 		}
 		if(Size>=nLen){
 			break;
 		}
-		t_user = millis() - t_begin;
+		#if defined(SCS_USE_MICROS)
+			t_user = micros() - t_begin;
+		#else
+			t_user = millis() - t_begin;
+		#endif
 		if(t_user>IOTimeOut){
 			break;
 		}
